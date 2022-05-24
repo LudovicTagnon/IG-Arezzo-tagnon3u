40a14401ca5849c1e3a80ec7e5caf95181297fd2
TAGNON Ludovic <ludovic.tagnon3@etu.univ-lorraine.fr>
2022-05-24 22:25:21 +0200

    Creation du bouton pour listview et fonction d'action liee au bouton

--- src/main/java/Arezzo/CentreController.java
+++ src/main/java/Arezzo/CentreController.java
@@ -3,12 +3,21 @@ package Arezzo;
 import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CentreController implements Observateur {
+
+    private ListView<Label> ListeNotes = new ListView<>();
 
     @FXML
     private ImageView Image_Partition = new ImageView();
@@ -17,8 +26,17 @@ public class CentreController implements Observateur {
         Data.getInstance().ajouterObservateur(this);
     }
 
+    @FXML
+    protected void afficherNotes() {
+        AnchorPane listviewPane = new AnchorPane(ListeNotes);
+        Stage stage = new Stage();
+        stage.setScene(new Scene(listviewPane, 300,800));
+        stage.show();
+    }
+
     @Override
     public void reagir() {
         this.Image_Partition.setImage(Data.getPartition().getImage());
+
     }
 }
